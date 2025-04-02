40d14f35c94aecc4722627a71c7f12266102e397
chimitslucie <chimitslucie@gmail.com>
2025-04-02 15:07:20 +0200

    add blog page, modification json + pattern

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,5 +1,44 @@
-function Blog() {
+import patternLight from "../Assets/Images/pattern-light.svg";
+import Nav from "../Components/Nav";
+import { Link } from "react-router";
+import datas from "../Data/data.json";
+import Footer from "../Components/Footer";
 
+function Blog() {
+    const formatDate = (isoString) => {
+        const date = new Date(isoString);
+        return date.toLocaleDateString("en-Us", { month: "long", day: "numeric", year: "numeric" });
+    };
+
+    return (
+        <div id="blog" className="blog">
+            <img src={patternLight} alt="forme de losange" className="right" />
+            <img src={patternLight} alt="forme de losange" className="left" />
+            <div className="container">
+                <Nav />
+                <div className="blogContent">
+                    <section className="blogContentHeader">
+                        <h1 className="title text2">My Articles</h1>
+                        <p className="text text7">Below are all my recent blog posts. Click on any title to read the full article.</p>
+                    </section>
+                    <hr />
+                    <section className="blogLinks">
+                        {datas.map((data, index) => (
+                            <div key={index}>
+                                <Link className="blogLink">
+                                    <h3 className="blogLinkTitle text5">{data.title}</h3>
+                                    <p className="blogLinkDate textItalic8">{formatDate(data.publishedAt)}</p>
+                                    <p className="blogLinkText text7">{data.description}</p>
+                                </Link>
+                                <hr />
+                            </div>
+                        ))}
+                    </section>
+                    <Footer />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Blog
